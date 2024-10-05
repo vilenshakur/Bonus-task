e590c9d228863147b00b6b91cb07b6ab029ac042
vilenshakur <125366281+vilenshakur@users.noreply.github.com>
2024-10-05 15:23:15 +0500

    Add files via upload

---
+++ task.6.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+//  int main(){
+//     int a, b;                //    1  гипотенуза 
+//     cin >> a >> b;
+//    cout << hypot(a,b);
+
+
+//    return 0;
+//  }
+
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     cout << "The next number for the number " << n << " is " << n + 1 << "." << endl;
+//     cout << "The previous number for the number " << n << " is " << n - 1 << "." << endl;       // 2 next and previous numbers
+
+
+//     return 0;
+// }
+
+// int main(){
+//      int a,b;
+//      cin >> a >> b;
+//      double result = static_cast<double>(b) / a;      // 3 Difinition of apple
+//      cout << floor(result) << endl;
+
+//      return 0;
+// }
+
+// int main()
+// {
+//     int a, b;
+//     cin >> a >> b;
+//     int remainder = b % a;         // 4 difinition of apple 2
+
+//     cout << remainder << endl;
+
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     int v, t;
+//     const int MKAD_LENGTH = 109;
+//     cin >> v >> t;
+ 
+//     int position = (v * t) % MKAD_LENGTH;       // 5 MKAD
+//     if (position < 0)
+//     {
+//         position += MKAD_LENGTH;
+//     }
+//     cout << position << endl;
+
+//     return 0;
+// }
+
+
+
+// int main()
+// {
+//     int number;
+
+    
+//     cin >> number;                             // 6 last number
+
+    
+//     int last_digit = number % 10;
+
+    
+//     cout << last_digit << endl;
+
+//     return 0;
+// }
+
+
+// int main(){
+//     int a;
+//     cin >> a;                                 // 7 ten number
+//     int ten = (a/10);
+//     cout << ten << endl;
+//     return 0;
+// }
+
+
+// int main(){
+//     int a;
+//     cin >> a;
+//     int second = (a/10) % 10;                 // 8 second number
+//     cout << second << endl;
+//     return 0;
+// }
+
+// int main(){
+//     int a;
+//     cin >> a;                                // 9 Summ of numbers
+//     int ten = (a/10);
+//     cout << ten << endl;
+//     return 0;
+// }
+ 
+
+//  int main(){
+//     int a;
+//     cin >> a;
+//     cout << a + 2 - (a % 2) << endl;          // 10 next even
+//     return 0;
+//  }
